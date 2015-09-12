565e42976cc769ed76f05de1a8265505f4f86911
Jason Mitchell <jasonmitchell@live.co.uk>
2015-09-12 18:55:24 +0100

    Adding QueryableRepository

---
+++ Sample.Repositories/QueryableRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace Sample.Repositories
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public class QueryableRepository
+    {
+        private readonly DbContext context;
+
+        public QueryableRepository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<T> Query<T>(Expression<Func<T, bool>> predicate = null) where T : class
+        {
+            var query = (IQueryable<T>)context.Set<T>();
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            return query;
+        }
+    }
+}
