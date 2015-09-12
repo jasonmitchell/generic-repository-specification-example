8b6a4218d57cf6ffafc700bfd2ff4bd912a56cb8
Jason Mitchell <jasonmitchell@live.co.uk>
2015-09-12 18:58:20 +0100

    Adding NonQueryableRepository

---
+++ Sample.Repositories/NonQueryableRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace Sample.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public class NonQueryableRepository
+    {
+        private readonly DbContext context;
+
+        public NonQueryableRepository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public ICollection<T> Query<T>(Expression<Func<T, bool>> predicate = null) where T : class
+        {
+            var query = (IQueryable<T>)context.Set<T>();
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            return query.ToList();
+        }
+    }
+}
